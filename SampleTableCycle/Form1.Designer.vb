ca20857b32bf970d8c6baefc6f32b2ff4c068d8f
Evgeniy909 <Jonny_omsk@mail.ru>
2021-07-19 11:10:25 +0500

    Первый рабочий вариант без извлечения значений

--- SampleTableCycle/Form1.Designer.vb
+++ SampleTableCycle/Form1.Designer.vb
@@ -22,10 +22,93 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox2 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox3 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox4 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox5 = New System.Windows.Forms.CheckedListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.CheckOnClick = True
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6"})
+        Me.CheckedListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(120, 364)
+        Me.CheckedListBox1.TabIndex = 0
+        '
+        'CheckedListBox2
+        '
+        Me.CheckedListBox2.CheckOnClick = True
+        Me.CheckedListBox2.FormattingEnabled = True
+        Me.CheckedListBox2.Items.AddRange(New Object() {"a", "b", "c", "d", "e", "f"})
+        Me.CheckedListBox2.Location = New System.Drawing.Point(138, 12)
+        Me.CheckedListBox2.Name = "CheckedListBox2"
+        Me.CheckedListBox2.Size = New System.Drawing.Size(120, 364)
+        Me.CheckedListBox2.TabIndex = 1
+        '
+        'CheckedListBox3
+        '
+        Me.CheckedListBox3.CheckOnClick = True
+        Me.CheckedListBox3.FormattingEnabled = True
+        Me.CheckedListBox3.Items.AddRange(New Object() {"x", "y", "z"})
+        Me.CheckedListBox3.Location = New System.Drawing.Point(264, 12)
+        Me.CheckedListBox3.Name = "CheckedListBox3"
+        Me.CheckedListBox3.Size = New System.Drawing.Size(120, 364)
+        Me.CheckedListBox3.TabIndex = 2
+        '
+        'CheckedListBox4
+        '
+        Me.CheckedListBox4.CheckOnClick = True
+        Me.CheckedListBox4.FormattingEnabled = True
+        Me.CheckedListBox4.Items.AddRange(New Object() {"r", "t", "y", "u"})
+        Me.CheckedListBox4.Location = New System.Drawing.Point(390, 12)
+        Me.CheckedListBox4.Name = "CheckedListBox4"
+        Me.CheckedListBox4.Size = New System.Drawing.Size(120, 364)
+        Me.CheckedListBox4.TabIndex = 3
+        '
+        'CheckedListBox5
+        '
+        Me.CheckedListBox5.CheckOnClick = True
+        Me.CheckedListBox5.FormattingEnabled = True
+        Me.CheckedListBox5.Location = New System.Drawing.Point(516, 12)
+        Me.CheckedListBox5.Name = "CheckedListBox5"
+        Me.CheckedListBox5.Size = New System.Drawing.Size(120, 364)
+        Me.CheckedListBox5.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(561, 382)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(650, 418)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckedListBox5)
+        Me.Controls.Add(Me.CheckedListBox4)
+        Me.Controls.Add(Me.CheckedListBox3)
+        Me.Controls.Add(Me.CheckedListBox2)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents CheckedListBox2 As CheckedListBox
+    Friend WithEvents CheckedListBox3 As CheckedListBox
+    Friend WithEvents CheckedListBox4 As CheckedListBox
+    Friend WithEvents CheckedListBox5 As CheckedListBox
+    Friend WithEvents Button1 As Button
 End Class
--- SampleTableCycle/Form1.vb
+++ SampleTableCycle/Form1.vb
@@ -1,3 +1,55 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim Dict As New Dictionary(Of String, Integer)
+        Dim multiply As Integer = 1
+
+        For Each TheCLB As CheckedListBox In Me.Controls.OfType(Of CheckedListBox)
+            If TheCLB.CheckedItems.Count > 0 Then
+                multiply *= TheCLB.CheckedItems.Count
+                Dict.Add(TheCLB.Name, TheCLB.CheckedItems.Count)
+            End If
+        Next
+
+        Dim Dt As New DataTable
+
+        If Dict.Count <> 0 Then
+            'Подготовка столбцов
+            Dt.Columns.Add("0", GetType(Integer))
+            Dt.Columns.Add("1", GetType(Integer))
+            Dt.Columns.Add("2", GetType(Integer))
+            Dt.Columns.Add("3", GetType(Integer))
+            '________
+
+            'создадим необходимое количество строк в таблице
+            Dim nRow As DataRow
+            For i = 0 To multiply - 1
+                nRow = Dt.NewRow()
+                Dt.Rows.Add(nRow)
+            Next
+
+            Dim nrR As Integer = 0
+            Dim nrC As Integer = 0
+
+            For Each i As Integer In Dict.Values
+
+                Dim x As Integer = multiply / i
+                Dim _x As Integer = x - 1
+
+                For v As Integer = 0 To _x
+                    For c As Integer = 0 To i - 1
+                        Dt.Rows.Item(nrR).Item(nrC) = Dict.Values.ElementAt(c)
+                        nrR += 1
+                    Next c
+                Next v
+                nrC += 1
+                nrR = 0
+            Next
+        End If
+
+    End Sub
 End Class
